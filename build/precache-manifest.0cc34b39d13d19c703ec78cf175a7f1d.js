52eb412b6ccdc4404cb70e71ed50dadae0053edd
Kshitij Mhatre <kmhatre@yodlee.com>
2019-11-07 23:54:00 +0530

    built

---
+++ build/precache-manifest.0cc34b39d13d19c703ec78cf175a7f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bf6ce28d20cac98929fb221f523a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b4dc2ea1f7029f86732",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4c7dfceb29875cbf54d1",
+    "url": "/static/js/2.dc372d06.chunk.js"
+  },
+  {
+    "revision": "7b4dc2ea1f7029f86732",
+    "url": "/static/js/main.e03763b1.chunk.js"
+  },
+  {
+    "revision": "bf1d21071788f2fa2cc6",
+    "url": "/static/js/runtime-main.dc809e1b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
